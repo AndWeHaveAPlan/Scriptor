e65c13b2ecf9c0a9996648ade2782c9f8ec886f7
Evgeny Snitko <ravenor@ravenor.ru>
2019-08-14 15:41:11 +0300

    using fix

--- Scriptor.AspExtensions/AspExtensions.cs
+++ Scriptor.AspExtensions/AspExtensions.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AndWeHaveAPlan.Scriptor.AspExtensions.Providers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Scriptor.AspExtensions.Providers;
 
 namespace AndWeHaveAPlan.Scriptor.AspExtensions
 {
